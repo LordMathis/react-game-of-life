9147603e40eba1b77ec30f43e67bf1e5578174b2
LordMathis <namesny.matus@gmail.com>
2017-08-01 21:06:37 +0200

    Update styles

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,6 +6,7 @@ const Board = (props) => (
     <canvas id="canvas" width={props.width * 10} height={props.height * 10} onClick={props.onClick}>
     </canvas>
     <div className="button-row">
+
       <button className="btn btn-default" onClick={props.onPause}>
         <i className={props.interval ? "glyphicon glyphicon-pause" : "glyphicon glyphicon-play"} aria-hidden="true"></i>
       </button>
@@ -13,26 +14,36 @@ const Board = (props) => (
         {'Generations: ' + props.generation}
       </label>
       <button className="btn btn-default" onClick={props.onSettings}>
-        <i className="glyphicon glyphicon-cog" aria-hidden="true"></i>
+        <i className="glyphicon glyphicon-cog" aria-hidden="true"> </i>
       </button>
-    </div>
-    <div className={props.showSettings ? "" : "hidden"}>
-      <div className="list-group col-md-3">
-        <button className="btn btn-default list-group-item" onClick={props.onClear}>
+
+      <div className={props.showSettings ? "dropdown-content" : "dropdown-content hidden"}>
+        <button className="btn btn-default dropdown-item" onClick={props.onClear}>
           Clear
         </button>
-        <button className="btn btn-default list-group-item" onClick={props.onGenerate} disabled={props.interval}>
+        <button className="btn btn-default dropdown-item" onClick={props.onGenerate} disabled={props.interval}>
           Generate
         </button>
-        <div className="list-group-item">
-          <label className="label label-primary label-gen">
+        <div className="dropdown-item">
+          <label className="label label-default label-gen">
             {'Speed: ' + props.speed}
           </label>
-          <input type="range" className="list-group-item speed-range" value={props.speed} min={10} max={1000} onChange={props.onSpeedUpdate}></input>
+          <input type="range" className="speed-range" value={props.speed} min={10} max={1000} onChange={props.onSpeedUpdate}></input>
         </div>
-        <input type="text" className="list-group-item" value={props.heightToSet} onChange={props.onHeightUpdate}></input>
-        <input type="text" className="list-group-item" value={props.widthToSet} onChange={props.onWidthUpdate}></input>
+        <div className="dropdown-item">
+          <label className="label label-default label-gen">
+            {'Height:'}
+          </label>
+          <input type="text" className="" value={props.heightToSet} onChange={props.onHeightUpdate}></input>
+        </div>
+        <div className="dropdown-item">
+          <label className="label label-default label-gen">
+            {'Width:'}
+          </label>
+          <input type="text" className="" value={props.widthToSet} onChange={props.onWidthUpdate}></input>
+        </div>
       </div>
+
     </div>
   </div>
 )
