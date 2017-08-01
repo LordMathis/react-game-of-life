a7a886bac0b8e8787427c583daf79d087d034544
LordMathis <namesny.matus@gmail.com>
2017-08-01 21:32:02 +0200

    Styling settings div

--- src/components/Board.js
+++ src/components/Board.js
@@ -17,34 +17,34 @@ const Board = (props) => (
         <i className="glyphicon glyphicon-cog" aria-hidden="true"> </i>
       </button>
 
-      <div className={props.showSettings ? "dropdown-content" : "dropdown-content hidden"}>
-        <button className="btn btn-default dropdown-item" onClick={props.onClear}>
-          Clear
-        </button>
-        <button className="btn btn-default dropdown-item" onClick={props.onGenerate} disabled={props.interval}>
-          Generate
-        </button>
-        <div className="dropdown-item">
-          <label className="label label-default label-gen">
-            {'Speed: ' + props.speed}
-          </label>
-          <input type="range" className="speed-range" value={props.speed} min={10} max={1000} onChange={props.onSpeedUpdate}></input>
-        </div>
-        <div className="dropdown-item">
-          <label className="label label-default label-gen">
-            {'Height:'}
-          </label>
-          <input type="text" className="" value={props.heightToSet} onChange={props.onHeightUpdate}></input>
-        </div>
-        <div className="dropdown-item">
-          <label className="label label-default label-gen">
-            {'Width:'}
-          </label>
-          <input type="text" className="" value={props.widthToSet} onChange={props.onWidthUpdate}></input>
-        </div>
+  </div>
+    <div className={props.showSettings ? "dropdown-content" : "dropdown-content hidden"}>
+      <button className="btn btn-default dropdown-item" onClick={props.onClear}>
+        Clear
+      </button>
+      <button className="btn btn-default dropdown-item" onClick={props.onGenerate} disabled={props.interval}>
+        Generate
+      </button>
+      <div className="dropdown-item">
+        <label className="label label-default label-gen">
+          {'Speed: ' + props.speed}
+        </label>
+        <input type="range" className="speed-range" value={props.speed} min={10} max={1000} onChange={props.onSpeedUpdate}></input>
       </div>
-
+      <div className="dropdown-item">
+        <label className="label label-default label-gen">
+          {'Height:'}
+        </label>
+        <input type="text" className="" value={props.heightToSet} onChange={props.onHeightUpdate}></input>
+      </div>
+      <div className="dropdown-item">
+        <label className="label label-default label-gen">
+          {'Width:'}
+        </label>
+        <input type="text" className="" value={props.widthToSet} onChange={props.onWidthUpdate}></input>
+      </div>
     </div>
+
   </div>
 )
 
